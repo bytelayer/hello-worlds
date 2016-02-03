1d7aae1683cf05f2011f619a5a0a0afd36d52113
Magnus Mischel <magnus@misec.net>
2016-02-03 22:15:35 +0100

    Added C example

---
+++ C/helloworld.c
@@ -0,0 +1,12 @@
+/* Hello World example in C
+
+   Compile on Linux using gcc with this command:
+   gcc -o hello helloworld.c
+
+*/
+
+#include <stdio.h>
+
+int main(void) {
+  printf("Hello world!\n");
+}
