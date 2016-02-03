0e8e9cef4e9847c90fec0272ee7945c42df2caf4
Magnus Mischel <magnus@misec.net>
2016-02-04 00:35:42 +0100

    Added comments

--- Assembler/helloworld.asm
+++ Assembler/helloworld.asm
@@ -5,13 +5,13 @@ section .text
 	global _start		; This is the _start label - it must be named exactly _start for the linker to recognize the program entry point
 	
 _start:	
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, msg
-	mov rdx, 14
-	syscall
-	mov rax, 60
-	mov rdi, 0
+	mov rax, 1		; sys_write syscall (syscall number 1)
+	mov rdi, 1		; fd = 1 (stdout)
+	mov rsi, msg		; source index = msg
+	mov rdx, 14		; string length
+	syscall			; execute sys_write
+	mov rax, 60		; sys_exit syscall (syscall number 60)
+	mov rdi, 0		; exit code = 0
 	syscall
 
 	
