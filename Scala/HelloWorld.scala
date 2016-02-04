8618b7b2e4d8bcd5e21524a673b7f5f690d161ce
Magnus Mischel <magnus@misec.net>
2016-02-04 10:16:38 +0100

    Added Scala

---
+++ Scala/HelloWorld.scala
@@ -0,0 +1,8 @@
+/** Install with sudo apt-get install scala
+    Compile with scalac HelloWorld.scala 
+    Run with scala HelloWorld
+*/
+
+object HelloWorld extends App {
+  println("Hello world!")
+}
