c78a7782ea35e8d94dd0d15d4918d546bc505aea
Magnus Mischel <magnus@misec.net>
2016-02-05 15:32:03 +0100

    Added C#

--- Assembler/helloworld.asm
+++ Assembler/helloworld.asm
@@ -2,16 +2,15 @@ section .data
 	msg db "Hello world!", 13, 10, 0
 
 section .text
-	global _start		; This is the _start label - it must be named exactly _start for the linker to recognize the program entry point
-	
-_start:	
-	mov rax, 1		; sys_write syscall (syscall number 1)
-	mov rdi, 1		; fd = 1 (stdout)
-	mov rsi, msg		; source index = msg
-	mov rdx, 14		; string length
-	syscall			; execute sys_write
-	mov rax, 60		; sys_exit syscall (syscall number 60)
-	mov rdi, 0		; exit code = 0
-	syscall
+	global _start		; This is the _start label - it must be named exactly _start
+	 								; for the linker to recognize the program entry point
 
-	
+_start:
+	mov rax, 1			; sys_write syscall (syscall number 1)
+	mov rdi, 1			; fd = 1 (stdout)
+	mov rsi, msg		; source index = msg
+	mov rdx, 14			; string length
+	syscall					; execute sys_write
+	mov rax, 60			; sys_exit syscall (syscall number 60)
+	mov rdi, 0			; exit code = 0
+	syscall
